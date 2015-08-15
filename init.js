68bc02e913efd5265a96f48cf012fec2f1155057
elklein96 <elklein96@gmail.com>
2015-08-14 22:24:20 -0400

    unix cron

--- init.js
+++ init.js
@@ -2,14 +2,15 @@ var request = require('request');
 var fs = require('fs');
 var spawn = require('child_process').spawn;
 var crontab = require('node-crontab');
-
-var browser;
 var child = spawn('node', ['app.js']);
 
-process.platform === "win32" ? browser = spawn('start', ['http://localhost:8888']) : browser = spawn('open', ['http://localhost:8888']);
+if(process.platform === "win32"){
+	spawn('start', ['http://localhost:8888'])
+}else{
+	spawn('open', ['http://localhost:8888']);
+	spawn('find', ['./public/downloads', '-type', 'f', '-mmin', '+59', '-exec', 'rm', '-f', '{}', '\\;']);
+}
 
 child.stdout.on('data', function(data) {
   console.log(data.toString());
 });
-
-spawn('find', ['./public/downloads', '-type', 'f', '-mmin', '+59', '-exec', 'rm', '-f', '{}', '\\;']);
