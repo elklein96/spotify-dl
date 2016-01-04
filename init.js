40e094e491ef052dd2377887af249ca794d01ddf
elklein96 <elklein96@gmail.com>
2016-01-04 14:25:52 -0500

    Fixed windows boot execution issue

--- init.js
+++ init.js
@@ -4,12 +4,14 @@ var spawn = require('child_process').spawn;
 var crontab = require('node-crontab');
 var child = spawn('node', ['app.js']);
 
-if(process.platform === "win32"){
+if(process.platform.indexOf("win") > -1){
 	spawn('start', ['http://localhost:8888'])
 }else{
 	spawn('open', ['http://localhost:8888']);
 	spawn('find', ['./public/downloads', '-type', 'f', '-mmin', '+59', '-exec', 'rm', '-f', '{}', '\\;']);
 }
+
+console.log(process.platform);
 
 child.stdout.on('data', function(data) {
   console.log(data.toString());
